9a0d436b415be72973c6325132a65c6194f0a9fa
Filipe <filipe.b.braga@gmail.com>
2021-10-26 17:52:19 -0300

    up to 11

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.getElementById('criar-tarefa').addEventListener('click', function () {
   let elementText = document.getElementById('texto-tarefa').value;
   element.innerText = elementText;
   element.onclick = myFunction;
+  element.ondblclick = myFunction2;
   olElement.appendChild(element);
   document.getElementById('texto-tarefa').value = '';
 });
@@ -13,7 +14,28 @@ function myFunction() {
   if (this.classList.contains('selected')) {
     this.classList.remove('selected');
   } else {
-    document.querySelectorAll('li').forEach(e => e.classList.remove('selected'));
+    document
+      .querySelectorAll('li')
+      .forEach((e) => e.classList.remove('selected'));
     this.classList.add('selected');
   }
 }
+
+function myFunction2() {
+  if (this.classList.contains('completed')) {
+    this.classList.remove('completed');
+  } else {
+    this.classList.add('completed');
+  }
+}
+
+document.getElementById('apaga-tudo').addEventListener('click', function () {
+  document.getElementById('lista-tarefas').innerHTML = '';
+});
+
+document.getElementById('remover-finalizados').addEventListener('click', function () {
+    const elements = document.getElementsByClassName('completed');
+    while (elements.length > 0) {
+      elements[0].parentNode.removeChild(elements[0]);
+    }
+});
